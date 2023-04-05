ad491fe85da1717a7a7b184f5cc4f87956a2f72f
Galuh Putra Warman <galuhputraww@gmail.com>
2023-04-05 17:00:07 +0700

    menambahkan list

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote From Local to GITHUB</title>
+</head>
+<body>
+    <P>MARI KITA BUAT LOCAL KE GITHUB</P>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed iure corrupti iusto pariatur at. Ducimus perspiciatis temporibus nihil hic? Nemo fugiat, ea at vero velit pariatur nihil similique! Ipsa?</p>
+    <ul>
+        <li>libxml_get_errors</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eaque tenetur rem quia? Quia consectetur quidem beatae. Ipsum modi similique non eius, eos consectetur ex perspiciatis aspernatur laborum dolorem enim?</li>
+    </ul>
+</body>
+</html>
